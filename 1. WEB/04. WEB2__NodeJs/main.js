6fb9b708312e779d4fca3adb7cdbae69b212b4ba
201511094 <kws2205@naver.com>
2020-02-10 17:08:19 +0900

    Modify main.js adding welcome page

--- 1. WEB/04. WEB2__NodeJs/main.js
+++ 1. WEB/04. WEB2__NodeJs/main.js
@@ -1,44 +1,77 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-
-var app = http.createServer(function(request,response){
+ 
+var app = http.createServer(function(request,response)
+{
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    console.log(url.parse(_url, true));
     var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;   //meaningful word
-
-    if (pathname === '/') {
-        //undefined problem => modify `data/${queryData.id}`
-        fs.readFile(`${__dirname}/data/${queryData.id}`, 'utf8', function(err, description) {
-        var template = `
-            <!doctype html>
-            <html>
-            <head>
+    if (pathname === '/')
+    {
+        if (queryData.id === undefined)
+        {
+            //HOME
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description)
+                {
+                  var title = 'Welcome';
+                  var description = 'Hello, Node.js';
+                  var template = `
+                      <!doctype html>
+                      <html>
+                      <head>
+                        <title>WEB1 - ${title}</title>
+                        <meta charset="utf-8">
+                      </head>
+                      <body>
+                        <h1><a href="/">WEB</a></h1>
+                        <ul>
+                          <li><a href="/?id=HTML">HTML</a></li>
+                          <li><a href="/?id=CSS">CSS</a></li>
+                          <li><a href="/?id=JavaScript">JavaScript</a></li>
+                        </ul>
+                        <h2>${title}</h2>
+                        <p>${description}</p>
+                      </body>
+                      </html>
+                      `;
+                  response.writeHead(200);
+                  response.end(template);
+                });
+        }
+        else
+        {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description)
+            {
+              var title = queryData.id;
+              var template = `
+              <!doctype html>
+              <html>
+              <head>
                 <title>WEB1 - ${title}</title>
                 <meta charset="utf-8">
-            </head>
-            <body>
+              </head>
+              <body>
                 <h1><a href="/">WEB</a></h1>
-                <ol>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">JavaScript</a></li>
-                </ol>
+                <ul>
+                  <li><a href="/?id=HTML">HTML</a></li>
+                  <li><a href="/?id=CSS">CSS</a></li>
+                  <li><a href="/?id=JavaScript">JavaScript</a></li>
+                </ul>
                 <h2>${title}</h2>
                 <p>${description}</p>
-            </body>
-            </html>
-            `;
-
-        response.writeHead(200);
-        response.end(template);
-    })
-    } else {
-        response.writeHead(404);
-        response.end('Not found');
+              </body>
+              </html>
+              `;
+              response.writeHead(200);
+              response.end(template);
+            });
+        }
     }
-
+    else
+    {
+      response.writeHead(404);
+      response.end('Not found');
+    }
 });
 app.listen(3000);
