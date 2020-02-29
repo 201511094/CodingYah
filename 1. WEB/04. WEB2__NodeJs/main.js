ec067959e6e40326877fee16affb36d088c264c7
201511094 <kws2205@naver.com>
2020-02-29 20:55:29 +0900

    Modify main.js using module and add template.js in lib folder

--- 1. WEB/04. WEB2__NodeJs/main.js
+++ 1. WEB/04. WEB2__NodeJs/main.js
@@ -3,35 +3,8 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-var template = {
-  HTML: function(title, list, body, control) {
-    return `
-      <!doctype html>
-      <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">My WEB Page!</a></h1>
-          ${list}
-          ${control}
-          ${body}
-        </body>
-      </html>
-    `;
-  },
-  List: function(filelist) {
-    var list = `<ul>`;
-    var i = 0;
-    while (i < filelist.length) {
-      list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-      i++;
-    }
-    list += `</ul>`
-    return list;
-  }
-}
+//module
+var template = require('./lib/template.js');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
